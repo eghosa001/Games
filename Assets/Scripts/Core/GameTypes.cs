a6b68facef8b804c3ef5b69ee587c4185dd7dbfb
eghosa001 <aighewieghosa111@gmail.com>
2026-09-02 09:14:59 +0100

    feat: add core game types

---
+++ Assets/Scripts/Core/GameTypes.cs
@@ -0,0 +1,46 @@
+namespace Renew.Core
+{
+    public enum PropertyType
+    {
+        Warehouse,
+        Factory,
+        RetailOutlet
+    }
+
+    public enum IndustryType
+    {
+        Manufacturing,
+        Logistics,
+        Retail
+    }
+
+    public enum ResourceType
+    {
+        Timber,
+        Iron,
+        Energy,
+        Food,
+        Technology
+    }
+
+    public enum RestorationStage
+    {
+        Neglected,
+        Cleaned,
+        Repaired,
+        Rebuilt,
+        Installed,
+        Designed,
+        Operational
+    }
+
+    public enum CompanyStage
+    {
+        Founded,
+        Expanded,
+        Partnered,
+        Financed,
+        Corporation,
+        GlobalCorporation
+    }
+}
